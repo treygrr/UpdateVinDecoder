00fb881ba520dcf869c6a590272303e191191b6e
Trey <phlasc@gmail.com>
2020-11-06 13:44:03 -0500

    Add algo for data dump

--- GetCounts.js
+++ GetCounts.js
@@ -23,10 +23,14 @@ const GetArticles = async (model_id) => {
     return result;
 }
 
-const GetAttachments = async (article_id) => {
-    const query = `select * from articles_attachments where article_id = ${article_id}`;
+const GetAttachments = async (model_id) => {
+    const query = `select count(*) from article_attachments_pg where article_id in (select id from articles_pg where model = ${model_id} and status <> 6);`;
     const result = await pg.query(query); 
     return result;
+}
+
+const WriteFile = (fileToWrite, fileName) => {
+    fs.writeFileSync(fileName, JSON.stringify(fileToWrite));
 }
 
 /*
@@ -35,13 +39,35 @@ const GetAttachments = async (article_id) => {
             get articles
                 foreach articles
                     get attachements
+        saveData
 
 
 
 */
-
-
 pg.connect();
 
+const run = async () => {
+    let dataset = {};
+    let models = await GetModels().then(res => res.rows).catch(err => console.log(err));
+    for (const model in models) {
+        // console.log(models[model].name);
+        let articles = await GetArticles(models[model].id).then(res => res.rows).catch(err => console.log(err));
+        let articlesattachmentcount = 0;
 
-fs.writeFileSync(`ExportedArticleData.text`, JSON.stringify(currentFromFail));
+        let attachements = await GetAttachments(models[model].id).then(res => res.rows[0].count).catch(err => console.log(err));
+
+        let modelName = models[model].name.toString();
+
+        dataset[modelName] = {
+            "Articles Count: ": articles.length,
+            "Attachments Count": attachements
+        };
+    }
+ 
+    WriteFile(dataset, 'exportedData.txt');
+    console.log('Done!');
+    exit();
+}
+
+
+run();
